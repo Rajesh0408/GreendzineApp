7ba52adb4cf41ce3ad1d78816b8937246e910f43
unknown <rajesh.a0408@gmail.com>
2024-03-15 17:30:20 +0530

    completed bottom navigation bar

--- app/src/main/java/com/example/greendzineassignment/MainActivity.kt
+++ app/src/main/java/com/example/greendzineassignment/MainActivity.kt
@@ -3,19 +3,30 @@ package com.example.greendzineassignment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import com.example.greendzineassignment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val loginButton = findViewById<Button>(R.id.loginBtn)
+        // Find the login button using View Binding
+        val loginButton: Button = binding.loginBtn
+        //val loginButton = findViewById<Button>(R.id.loginBtn)
         loginButton.setOnClickListener{
-            val Intent = Intent(this, DashBoardActivity::class.java)
+            val Intent = Intent(this, MainActivity2::class.java)
             startActivity(Intent)
+
         }
+
+
     }
 
 
---
+++ app/src/main/java/com/example/greendzineassignment/MainActivity2.kt
@@ -0,0 +1,42 @@
+package com.example.greendzineassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.greendzineassignment.databinding.ActivityMain2Binding
+
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        replaceFragment(HomeFragment())
+
+
+        binding.BottomNavigationView.setOnNavigationItemSelectedListener  {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.employees -> replaceFragment(EmployeeFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.FrameLayout, fragment)
+            commit()
+
+        }
+}
