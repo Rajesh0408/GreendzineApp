bc2b78f79ac395ee35b87c0440606e3ad3d60fa8
unknown <rajesh.a0408@gmail.com>
2024-03-15 10:49:16 +0530

    completed UI design for login page

--- app/src/main/java/com/example/greendzineassignment/MainActivity.kt
+++ app/src/main/java/com/example/greendzineassignment/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.greendzineassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val loginButton = findViewById<Button>(R.id.loginBtn)
+        loginButton.setOnClickListener{
+            val Intent = Intent(this, DashBoardActivity::class.java)
+            startActivity(Intent)
+        }
     }
+
+
+
+
+
 }
